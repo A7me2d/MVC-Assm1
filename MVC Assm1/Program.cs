5e6b59d08c73d8428ee2c5fb07b0ae0f6374015f
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-09-07 22:44:00 +0300

    Map

--- MVC Assm1/Program.cs
+++ MVC Assm1/Program.cs
@@ -7,7 +7,22 @@ namespace MVC_Assm1
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+               endpoints.MapGet("/Home", async context =>
+               {
+                   await context.Response.WriteAsync("You are at home page");
+               });
+               
+               endpoints.MapGet("/Products", async context =>
+               {
+                   await context.Response.WriteAsync("you are at Products");
+               });
+            });
+
+            app.Run(async(HttpContext) => await HttpContext.Response.WriteAsync("NOT Found"));
+            //app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
