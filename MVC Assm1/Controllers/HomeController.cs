32cc6412f692f7b7599906460cf87c9561bd3943
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-09-08 12:06:02 +0300

    Action Result

---
+++ MVC Assm1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Assm1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {           
+            return RedirectToAction("AboutUs");
+        }
+        public string AboutUs()
+        {
+            return "this is About Us Action";
+        }
+    }
+}
